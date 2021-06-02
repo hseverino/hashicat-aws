8cd0abb078416c6686124106a360c29a6d063618
H Severino <hseverino@cargurus.com>
2021-06-02 19:44:10 +0000

    Added s3 bucket module.

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,7 +1,7 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
-  bucket_prefix = prefix
-  bucket = "my-s3-bucket"
+  bucket_prefix = "hseverino"
+  #bucket = "my-s3-bucket"
   acl    = "private"
 
   versioning = {
